d77e5c4df154ffd582ba67e668dc86afbc2a62d7
josekbm <jcblanco91@outlook.com>
2023-06-08 18:16:00 +0200

    deployment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Homepage } from './pages/Homepage';
 import { Search } from './pages/Search';
 import { Liked } from './pages/Liked';
 
 function App () {
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Homepage/>} />
                 <Route path="Search" element={<Search/>} />
                 <Route path="Liked" element={<Liked/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
     
