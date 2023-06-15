f8dd053b47ebc4ef906ec799fdd64713f837c3c0
josekbm <jcblanco91@outlook.com>
2023-06-15 14:44:09 +0200

    changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,8 +7,8 @@ export function Homepage() {
     <div className="App">
       <TopBar />
       <body className="App-body">
-        <p>Welcome to the PHOTO DASH aplication.<br></br>
-        Search among our almost 5 million photos library of our creators from all over the world. Save them in your favorites and download them for personal and comercial use.</p>
+        <p>Welcome to the PHOTO DASH aplication!<br></br>
+        Search among our images library of creators from all over the world. Save them in your favorites and download them for personal and comercial use absolutely free!!</p>
       </body>
     </div>
   );
